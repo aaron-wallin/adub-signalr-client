4fc9792f0bb49390796bcf08544fee824a1d97d3
aaron-wallin <aaron.wallin@gmail.com>
2018-08-14 16:10:41 -0700

    clean up unnecessary stuff

--- adub-signalr-client/Startup.cs
+++ adub-signalr-client/Startup.cs
@@ -22,39 +22,18 @@ namespace adub_signalr_sample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            //services.AddCors(options => options.AddPolicy("CorsPolicy", 
-            //builder => 
-            //{
-            //    builder.AllowAnyMethod().AllowAnyHeader()
-            //           .WithOrigins("https://adub-signalr-sample.apps.pcf.sandbox.cudirect.com")
-            //           .AllowCredentials();
-            //}));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             else
-            {
                 app.UseExceptionHandler("/Error");
-            }
-
+            
             app.UseStaticFiles();
-            app.UseCookiePolicy();
-            //app.UseCors("CorsPolicy");
             app.UseMvc();
         }
     }
